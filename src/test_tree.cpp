edb3ac2381318cdabc3b5e19add9fc935ebe8009
Gabriel <gpestre@users.noreply.github.com>
2020-04-23 14:51:36 -0400

    Update demo.

--- src/test_tree.cpp
+++ src/test_tree.cpp
@@ -9,13 +9,23 @@ int main(){
     TreeNode node1 = TreeNode();
     TreeNode node2 = TreeNode();
     TreeNode node3 = TreeNode();
+    TreeNode node4 = TreeNode();
     node1.setLeft(&node2);
     node1.setRight(&node3);
+    node2.setLeft(&node4);
 
     DecisionTree tree1(&node1);
 
     std::cout << "Tree size: ";
     std::cout << tree1.getSize();
+    std::cout << "" << std::endl;
+
+    std::cout << "Tree height: ";
+    std::cout << tree1.getHeight();
+    std::cout << "" << std::endl;
+
+    std::cout << "Node4 depth: ";
+    std::cout << node4.getDepth();
     std::cout << "" << std::endl;
 
     return 0;
