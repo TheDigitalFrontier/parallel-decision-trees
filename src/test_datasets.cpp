e721ab412ce2ca2ad68c856d8960d8b6952744b8
Gabriel <gpestre@users.noreply.github.com>
2020-05-01 02:58:01 -0400

    Update tests.

--- src/test_datasets.cpp
+++ src/test_datasets.cpp
@@ -20,15 +20,20 @@ int main(){
     DataVector* test_row = df_test.row(-1);
     std::cout << *test_row << std::endl;
 
-    // Get last column:
+    // Get copy of last column:
     std::cout << "Print last column:" << std::endl;
-    DataVector* test_col = df_test.col(-1);
+    DataVector* test_col = df_test.col(-1)->copy();
     std::cout << *test_col << std::endl;
     
     // Get transpose of first column:
     std::cout << "Print transpose of first column:" << std::endl;
     DataVector* test_transpose_col = df_test.col(0)->transpose();
     std::cout << *test_transpose_col << std::endl;
+    
+    // Get copy of data frame:
+    std::cout << "Print copy of data frame:" << std::endl;
+    DataFrame test_copy_df = df_test.copy();
+    std::cout << test_copy_df << std::endl;
     
     // Get transpose of data frame:
     std::cout << "Print transpose of data frame:" << std::endl;
