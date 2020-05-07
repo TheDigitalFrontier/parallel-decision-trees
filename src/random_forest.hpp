998fc9a386efe444cca762bf46a9c00211548e12
Gabriel <gpestre@users.noreply.github.com>
2020-05-07 00:48:53 -0400

    Implement RandomForest.

--- src/random_forest.hpp
+++ src/random_forest.hpp
@@ -6,5 +6,49 @@
 #include "datasets.hpp"
 #include "losses.hpp"
 
+class RandomForest
+{
+private:
+
+    // Attributes:
+    std::vector<DecisionTree> trees_;  // A list of fitted decision trees.
+    int num_trees_;  // Number of treeds to fit.
+    DataFrame dataframe_;  // Training data.
+    bool regression_;  // Use regression==false for a classification tree.
+    std::string loss_;  // String indicating loss function method.
+    int mtry_;  // Hyperparameter: Number of features to use at each split (or -1 for all in deterministic order; or 0 for sqrt(n_columns) ).
+    int max_height_;  // Stopping condition: max height of tree.
+    int max_leaves_;  // Stopping condition: max number of leaves.
+    int min_obs_;  // Stopping condition: minimum number of observations in a leaf.
+    int max_prop_;  // Stopping condition: minimum proportion of majority class in a leaf.
+    int num_features_;  // State variable: Number of features in dataset.
+    bool fitted_;  // State variable: Flag indicated whether or not the random forest has been trained.
+    int seed_;  // Metaseed for random seed generator.
+    SeedGenerator seed_gen;  // Random seed generator.
+
+public:
+
+    // Constructors:
+    RandomForest(
+        DataFrame dataframe, int num_trees, bool regression, std::string loss, int mtry=-1, 
+        int max_height=-1, int max_leaves=-1, int min_obs=-1, double max_prop=-1, int seed=-1
+    );
+
+    // Getters:
+    int getNumTrees() const;  // Number of trees in RandomForest.
+    bool isRegressionTree() const;  // Type of tree (classification or regression).
+    bool isFitted() const;  // Indicates whether the tree has been fitted on training data.
+    std::vector<DecisionTree> getTrees() const;  // Get a vector of the fitted trees.
+    DecisionTree getTree(int i) const;  // Get one of the fitted trees.
+    DataFrame getDataFrame() const;  // Training data.
+
+    // Setters:
+
+    // Utilities:
+    void fit();  // Perform fitting (using fit_ helper).
+    DataVector predict(DataFrame* testdata) const;  // Perform prediction sequentially on each observation.
+
+};
+
 
 #endif
--- tests/test_random_forest.cpp
+++ tests/test_random_forest.cpp
@@ -8,5 +8,60 @@
 
 int main(){
 
+    int num_trees = 10;
+
+    std::cout << "Define training data:" << std::endl;
+    DataFrame training_data = DataFrame({
+        {2.232, 2.456, 2.000, 0},
+        {2.232, 2.456, 3.000, 1},
+        {2.277, 8.735, 3.000, 2},
+        {2.965, 6.846, 3.000, 2},
+        {2.252, 6.452, 3.000, 2},
+        {2.222, 9.944, 3.000, 2},
+        {2.322, 8.747, 3.000, 2},
+        {2.322, 7.667, 3.000, 2},
+        {6.201, 6.342, 3.000, 3},
+        {6.201, 7.442, 3.000, 3},
+        {7.403, 9.944, 3.000, 3},
+        {8.720, 8.747, 3.000, 3},
+        {6.804, 9.941, 3.000, 3},
+        {6.201, 9.452, 3.000, 3},
+        {8.403, 3.944, 3.000, 4},
+        {8.403, 3.944, 4.000, 5},
+    });
+    training_data.print();
+
+    std::cout << "Define test data:" << std::endl;
+    DataFrame test_data = DataFrame({
+        {2.0, 0.0, 3.0},  // Expected: 1.
+        {2.0, 1.0, 3.0},  // Expected: 1.
+        {2.0, 2.0, 3.0},  // Expected: 1.
+        {2.0, 7.0, 3.0},  // Expected: 2.
+        {2.0, 8.0, 3.0},  // Expected: 2.
+        {2.0, 9.0, 3.0},  // Expected: 2.
+        {7.0, 7.0, 3.0},  // Expected: 3.
+        {7.0, 8.0, 3.0},  // Expected: 3.
+        {7.0, 9.0, 3.0},  // Expected: 3.
+        {7.0, 1.0, 3.0},  // Expected: 4.
+        {7.0, 2.0, 3.0},  // Expected: 4.
+    });
+    test_data.print();
+    
+    std::cout << "Build and train RandomForest for classification." << std::endl;
+    RandomForest rf_classification = RandomForest(training_data,num_trees,false,"gini_impurity",-1,-1,-1,-1,-1);
+    rf_classification.fit();
+    
+    std::cout << "Perform prediction on new data:" << std::endl;
+    DataVector pred_classification = rf_classification.predict(&test_data);
+    std::cout << pred_classification << std::endl;
+    
+    std::cout << "Build and train RandomForest for regression." << std::endl;
+    RandomForest rf_regression = RandomForest(training_data,num_trees,true,"mean_squared_error",-1,-1,-1,-1,-1);
+    rf_regression.fit();
+    
+    std::cout << "Perform prediction on new data:" << std::endl;
+    DataVector pred_regression = rf_regression.predict(&test_data);
+    std::cout << pred_regression << std::endl;
+
     return 0;
 };
