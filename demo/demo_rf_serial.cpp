4f952ab53f7f2c32c387c9825dcc93e576e9b04a
Johannes Kolberg <johannes.kolberg@cognite.com>
2020-05-08 18:27:55 +0200

    Wrap up RF demo v1

--- demo/demo_rf_serial.cpp
+++ demo/demo_rf_serial.cpp
@@ -23,20 +23,25 @@ int main(){
     // Fit RF on full dataset and verify accuracy is high on seen data
     std::cout << "Build and fit RandomForest binary classifier on Sonar" << std::endl;
 
-    RandomForest rf1 = RandomForest(df_sonar, ntree, false, "gini_impurity", -1, -1, -1, -1, -1, 1337);
+    RandomForest rf1 = RandomForest(df_sonar,ntree,false,"gini_impurity",-1,-1,-1,-1,-1,1337);
     rf1.fit();
 
-    std::cout << "Tree fitted:" << rf1.isFitted() << std::endl;
+    std::cout << "Tree fitted: " << rf1.isFitted() << std::endl;
 
     DataVector preds = rf1.predict(&df_sonar);
     std::cout << "Predictions size: " << preds.size() << std::endl;
-
-
-    //std::cout << "Accuracy on full training set: " << accuracy(targets, preds) << std::endl;
+    std::cout << "RF1 accuracy full train: " << accuracy(targets, preds) << std::endl;
     
-    /* std::cout << "Perform prediction on new data:" << std::endl;
-    DataVector pred_regression = rf_regression.predict(&test_data);
-    std::cout << pred_regression << std::endl; */
+    // Create train and test data
+    std::vector<DataFrame> train_and_test = df_sonar.train_test_split(0.2, 1337);
+    DataFrame df_train = train_and_test[0];
+    DataFrame df_test = train_and_test[1];
+
+    // Fit RF on train and evalute on test, with lower accuracy
+    RandomForest rf2 = RandomForest(df_train,ntree,false,"gini_impurity",-1,-1,-1,-1,-1,1337);
+
+    std::cout << "RF2 accuracy train: " << accuracy(df_train.col(-1), rf2.predict(&df_train)) << std::endl;
+    std::cout << "RF2 accuracy test: " << accuracy(df_test.col(-1), rf2.predict(&df_test)) << std::endl;
 
     return 0;
 };
