555ccdbc2d834cc71c3f9233146d1f49e447ef8c
Hardik Gupta <hardikgupta@g.harvard.edu>
2020-05-09 23:24:06 -0400

    adding cancer and hmeq datasets

---
+++ tests/test_hmeq_data.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../src/datasets.cpp"
+
+int main(){
+
+    DataLoader csv_loader = DataLoader("../data/hmeq_clean.csv");  // Load CSV test data.
+    DataFrame csv_test = csv_loader.load();
+    // csv_test.print();
+    DataVector* test_row = csv_test.row(-1);
+    std::cout << *test_row << std::endl;
+    
+};
